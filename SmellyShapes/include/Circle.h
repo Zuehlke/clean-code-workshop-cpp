78c9519dbfd33abf84fd1db08da895638c66387e
Arne Mertz <arne.mertz@zuehlke.com>
2022-11-02 10:14:04 +0100

    03 (redundant comments) 02: remove redundant comments in Circle (manual)

--- SmellyShapes/include/Circle.h
+++ SmellyShapes/include/Circle.h
@@ -12,17 +12,7 @@ public:
   bool contains(int x, int y);
   int countContainingPoints(int *xCoords, int *yCoords, int size);
 
-  /*!
-          \brief	This method returns the shape color.
-          \return	the shape color
-  */
   Color getColor();
-
-  /*!
-          \brief	Sets the shape color
-          \param color The color to set
-          \return	color the new color of the shape
-  */
   void setColor(Color color);
   int getX() const;
   int getY() const;
